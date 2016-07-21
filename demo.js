75ebb47cd67f5afb204dfaa148feeca408b596b1
Oleg Berman <bermanoleg@gmail.com>
2016-07-20 22:43:29 -0400

    pattern matching

--- demo.js
+++ demo.js
@@ -3,12 +3,21 @@ router.init();
 
 router
 
-.message('hello', () => {
-  router.speak('hey');
+.message('my name is @', (name) => {
+  router.speak('nice to meet you, ' + name);
 })
 .message('sup', () => {
   router.speak('not much');
 })
 .message('help', () => {
   router.speakOptions()
+})
+.message('lol', () => {
+  router.speak('fuck off.');
+})
+.message('ha', () => {
+  router.speak('fuck off you are annoying me .');
+})
+.message('fuck @', (str1) => {
+  console.log('fuck who?' + str1)
 })
--- lib.js
+++ lib.js
@@ -29,15 +29,59 @@ router.speakOptions = function() {
 }
 
 router.commandNotFound = function() {
-  router.speak('No such command, try again.')
+  router.speak('No such command.')
+}
+
+var extract_params = function(sbj_arr, tpl_arr, ok_char) {
+  var ok = true;
+  var out = [];
+  if(sbj_arr.length !== tpl_arr.length) return
+  for(var i = 0; i < sbj_arr.length; i++) {
+  	if(sbj_arr[i] !== tpl_arr[i]) {
+    	if(sbj_arr[i] !== ok_char) {
+      	out.push(sbj_arr[i])
+      }
+    }
+  }
+  return out;
+}
+
+
+var parseRoutesWithParams = function(input) {
+  // break down input into words
+  var words = input.split(' ')
+  if(words.length < 2) return
+  // now try to match with message patterns
+  // find all registered messages with dynamic input @
+  var templates = []
+  for(var key in router.commands) {
+    if(key.indexOf('@') !== -1) {
+      templates.push(key)
+    }
+  }
+  if(!templates.length) return
+  var out = []
+  templates.forEach(function(template) {
+    var tplwords = template.toLowerCase().split(' ')
+    if(tplwords.length !== words.length) return
+    out = extract_params(words, tplwords, '@')
+  })
+  return out
 }
 
 router.process = function() {
+  var params = parseRoutesWithParams(router.currentInput)
   var trimmed = router.currentInput.trim();
+  if(params) {
+    // replace evething back to @ to find match in commands
+    params.forEach(function(param) {
+      trimmed = trimmed.replace(param, '@')
+    })
+  }
   if(router.commands[trimmed]) {
-    router.commands[trimmed].call();
+    router.commands[trimmed].apply(this, params);
   } else {
-    router.commandNotFound()
+    router.commandNotFound();
   }
   router.currentInput = '';
 }
@@ -48,4 +92,5 @@ router.speak = function(text, done) {
   u.rate = 1.0;
   u.onend = done;
   speechSynthesis.speak(u);
+  return router;
 }
